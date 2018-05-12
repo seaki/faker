f823514e5649b06e78f48f856b769775508166cf
Se AKi <se-aki-personal@sastudio.jp>
2018-05-12 14:05:08 +0900

    Write a test

---
+++ test/test_faker_prettyrhythm.rb
@@ -0,0 +1,22 @@
+
+ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+ class TestFakerPrettyRhythm < Test::Unit::TestCase
+
+   def setup
+     @tester = Faker::PrettyRhythm
+   end
+
+   def test_character
+     assert @tester.character.match(/\w+/)
+   end
+
+   def test_location
+     assert @tester.location.match(/\w+/)
+   end
+
+   def test_quote
+     assert @tester.quote.match(/\w+/)
+   end
+
+ end
